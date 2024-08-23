aa56b908f4982e61f9137795fdcff760ba51d3c6
sharawi <adnanmish30@gmail.com>
2024-08-23 18:09:10 +0000

    "Refactor projectsPageContent component: updated function signature, removed styles, and modified JSX structure"

--- src/components/projectsPageContent.jsx
+++ src/components/projectsPageContent.jsx
@@ -5,51 +5,40 @@ import Showcase from "./imgSlider";
 import ProjectGithub from "./projectGithub";
 import "animate.css";
 
-const PageContent = (pageItems) => {
+const PageContent = ({ pageContent }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const sliderRef = useRef(null);
 
-  // Center the active image smoothly
   useEffect(() => {
     if (sliderRef.current) {
       const middleImage = sliderRef.current.querySelector(
         `.slide-${currentIndex}`
       );
       if (middleImage) {
-        const containerWidth = sliderRef.current.offsetWidth;
-        const imageWidth = middleImage.offsetWidth;
-        const scrollPosition =
-          middleImage.offsetLeft - containerWidth / 2 + imageWidth / 2;
-
-        // Add the smooth scrolling class
-        sliderRef.current.classList.add("scroll-smooth");
-        sliderRef.current.scrollTo({
-          left: scrollPosition,
-          behavior: "smooth",
-        });
-        // Remove the smooth scrolling class after the transition
         setTimeout(() => {
-          sliderRef.current.classList.remove("scroll-smooth");
-        }, 1000); // match the duration of your transition
+          middleImage.scrollIntoView({
+            behavior: "smooth",
+            block: "center",
+            inline: "center",
+          });
+        }, 100); // Adding a small delay can help with smooth transitions
       }
     }
   }, [currentIndex]);
 
-  // Define styles for the active and non-active images
-  const getSlideStyle = (index) => {
-    return index === currentIndex
-      ? "opacity-100 border-2 border-blue mx-8 z-10 transform scale-125 transition-all duration-500"
+  const getSlideStyle = (index) =>
+    index === currentIndex
+      ? "opacity-100 border-2 border-blue mx-8 z-10 transform transition-all duration-[2s]"
       : "opacity-30 scale-90";
-  };
 
-  const currentPageContent = pageItems.pageContent[currentIndex];
+  const currentPageContent = pageContent[currentIndex];
 
   return (
     <div className="w-full h-full flex justify-center items-center">
       <div className="w-1/2 h-full">
         <Background bgStyle={currentPageContent.bgStyle} />
-        <div className="flex justify-between *:z-50 px-16 -my-16">
-          <div className="">
+        <div className="flex justify-between px-16 -my-16">
+          <div>
             <Textbox
               title={currentPageContent.title}
               description={currentPageContent.description}
@@ -71,8 +60,7 @@ const PageContent = (pageItems) => {
             className="flex"
             onClick={() =>
               setCurrentIndex(
-                (currentIndex + pageItems.pageContent.length - 1) %
-                  pageItems.pageContent.length
+                (currentIndex + pageContent.length - 1) % pageContent.length
               )
             }
             aria-label="Previous slide"
@@ -85,42 +73,44 @@ const PageContent = (pageItems) => {
           <button
             className="flex"
             onClick={() =>
-              setCurrentIndex((currentIndex + 1) % pageItems.pageContent.length)
+              setCurrentIndex((currentIndex + 1) % pageContent.length)
             }
             aria-label="Next slide"
           >
             <span className="text-4xl text-blue opacity-50 hover:opacity-100">
-            Nxt{`>`}
+              Nxt{`>`}
             </span>
           </button>
         </div>
       </div>
 
-      <div className="w-1/2 h-full flex items-center justify-center pr-16">
-        <div
-          className="p-4 w-full h-full overflow-y-auto flex flex-col gap-4 items-center no-scrollbar"
-          aria-live="polite"
-          aria-atomic="true"
-          ref={sliderRef}
-          style={{ scrollSnapType: "x mandatory" }}
-        >
-          {pageItems.pageContent.map((image, index) => (
-            <img
-              key={index}
-              src={image.url}
-              alt={`Slide ${index}`}
-              className={`slide rounded w-4/5 h-64 object-cover transition-all duration-500 cursor-pointer slide-${index} ${getSlideStyle(
-                index
-              )}`}
-              data-index={index}
-              // style={{ scrollSnapAlign: "center" }}
-              onClick={() => setCurrentIndex(index)}
-            />
-          ))}
+      <div className="w-1/2 h-full flex items-center justify-between pr-16">
+        <div className="p-4 w-4/5 h-full overflow-y-auto overflow-x-hidden no-scrollbar">
+          <div
+            className="w-full h-3/5 flex flex-col gap-4 items-center"
+            aria-live="polite"
+            aria-atomic="true"
+            ref={sliderRef}
+            style={{ scrollSnapType: "y mandatory" }}
+          >
+            {pageContent.map((image, index) => (
+              <img
+                key={index}
+                src={image.url}
+                alt={`Slide ${index}`}
+                className={`slide w-full h-full object-cover transition-all duration-[2s] cursor-pointer slide-${index} ${getSlideStyle(
+                  index
+                )}`}
+                data-index={index}
+                style={{ scrollSnapAlign: "center" }}
+                onClick={() => setCurrentIndex(index)}
+              />
+            ))}
+          </div>
         </div>
 
         <div className="flex flex-col">
-          {pageItems.pageContent.map((image, index) => (
+          {pageContent.map((_, index) => (
             <span
               key={index}
               className={`mx-1 rounded-full my-2 cursor-pointer ${
